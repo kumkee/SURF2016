a8c975650fe446a5fe4ff0d4f66ea416aaddac88
Ubuntu <kumkeejjl@gmail.com>
2016-04-24 11:12:01 +0100

    adding PriceMatrix

--- src/pricematrix.py
+++ src/pricematrix.py
@@ -15,3 +15,34 @@ class PriceMatrix(CoinList):
 
     def __init__(self, start = time() - DAY, end = time(), period = HALF_HOUR):
 	super(PriceMatrix, self).__init__()
+	self._start = start
+	self._end = end
+	self._period = period
+	self.__coinFilter()
+
+	it = iter(self._coins)
+	coin = it.next()
+	chart = self.__getChart(coin)
+	self._chart = chart ######
+	cols = [d['date'] for d in chart]
+	self._pm = pd.DataFrame(index = self._coins, columns = cols)
+	self.__fillPriceRow(coin, chart)
+
+	print start, end, period
+
+
+    def __fillPriceRow(self, coin, chart):
+	self._pm.loc[coin] = [d['close'] for d in chart]
+
+
+    def __getChart(self, coin):
+	chart = self.polo.marketChart( \
+			pair = self._df.loc[coin]['pair'], \
+			start = self._start, \
+			end = self._end, \
+			period = self._period )
+	return chart
+
+    def __coinFilter(self):
+        self._coins = self.topNVolume( n = len(self.allActiveCoins()) / 3).index
+
