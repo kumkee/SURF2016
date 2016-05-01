0d60fd593666cb8cf21bc96eaa95a265b855714c
Ubuntu <kumkeejjl@gmail.com>
2016-05-01 09:55:07 +0100

    globalpricematrix

---
+++ src/globalpricematrix.py
@@ -0,0 +1,88 @@
+from coinlist import CoinList
+import pandas as pd
+from time import time
+from time import sleep
+import numpy as np
+
+NOW = 0
+FIVE_MINUTES = 60*5
+FIFTEEN_MINUTES = FIVE_MINUTES * 3
+HALF_HOUR = FIFTEEN_MINUTES * 2
+HOUR = HALF_HOUR * 2
+TWO_HOUR = HOUR * 2
+FOUR_HOUR = HOUR * 4
+DAY = HOUR * 24
+
+CSV_DEFAULT = 'pm.csv'
+
+
+class GlobalPriceMatrix(CoinList):
+
+    def __init__(self, start = DAY, end = NOW, period = HALF_HOUR, csv = None, coin_filter = 0.2):
+	super(GlobalPriceMatrix, self).__init__()
+	self._coin_filter = coin_filter
+	if csv:
+	    self.__getPriceFromFile(csv)
+	else:
+	    self.__getPriceFromExchange(start, end, period)
+
+
+    def __getPriceFromExchange(self, start, end, period):
+	t = time()
+	self._start = t - start
+	self._end = t - end + 10*period
+	self._period = period
+	self.__coinFilter()
+
+	coin = 'LTC'
+	chart = self.getChart(coin, start = self._start, end = self._end)
+	cols = [d['date'] for d in chart]
+	self._pm = pd.DataFrame(index = self._coins, columns = cols)
+	self.__fillPriceRow(coin, start = self._start, end = self._end)
+
+	for c in self._coins:
+	    if c == 'LTC':
+		continue
+	    self.__fillPriceRow(c, start = self._start, end = self._end)
+
+
+    def __fillPriceRow(self, coin, start, end):
+	chart = self.getChart(coin=coin, start=start, end=end)
+	for c in chart:
+	    self._pm.loc[coin, c['date']] = c['close']
+
+
+    @property
+    def pricematrix(self):
+	return self._pm
+
+
+    def getChart(self, coin, start, end):
+	chart = self.polo.marketChart( \
+			pair = self._df.loc[coin]['pair'], \
+			start = start, \
+			end = end, \
+			period = self._period )
+	return chart
+
+
+    def __coinFilter(self):
+	if(self._coin_filter):
+            self._coins = self.topNVolume(
+		n = int(len(self.allActiveCoins) * self._coin_filter)).index
+
+
+    def to_csv(self, filepath = CSV_DEFAULT):
+	#Save the database into csv file
+	pm = self._pm.transpose()
+	pm.index = pd.to_datetime(pm.index, unit = 's')
+	pm.to_csv(filepath)
+
+
+    def __getPriceFromFile(self, csv = CSV_DEFAULT):
+	pm = pd.DataFrame.from_csv(csv)
+	pm.index = pm.index.astype(np.int64)/10**9
+	self._pm = pm.transpose()
+	self._start = self._pm.columns[0]
+	self._end = self._pm.columns[-1]
+	self._period = self._pm.columns[1] - self._start
